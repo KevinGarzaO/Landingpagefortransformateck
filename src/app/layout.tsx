8caf514da1e47dc65119c21a4880094131ed121c
Kevin Garza <kevin_4569@msn.com>
2026-02-25 20:50:02 -0600

    Add Microsoft Clarity tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,6 +82,16 @@ export default function RootLayout({
             gtag('config', 'G-3RWYRZ63PV');
           `}
         </Script>
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vn7d9llhyi");
+          `}
+        </Script>
       </head>
       <body className="min-h-screen bg-black text-white antialiased">
         <MetaPixel />
